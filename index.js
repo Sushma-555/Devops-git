df999f7c582f9cc183d04ecdedb3ef52e5485ed3
Sushma-555 <152793327+Sushma-555@users.noreply.github.com>
2023-12-06 16:59:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+//Number taken is 5 or not
+/*let a=8
+if(a==5){
+    console.log("number is 5")
+}
+else{
+    console.log("number is not 5")
+}*/
+//Greatest of 2 numbers
+let a=10,b=5
+if(a>b)
+{
+    console.log("a value is greater than b")
+}
+else{
+    console.log("a value is not greater thhan b")
+}
+
+// 1.write a program to check whether a person is eligible to cast a voting or not
+//2.a program to check whether 22 is even or odd number
+//3.a program to check whether a person is eligible for IAS or not.>eligibility
+ criteria is age should be from 21 to 32
+ //4.a program to check whether a character is vowel or consonants.
+
